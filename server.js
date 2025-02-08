a44d2076ebc4c61767b46f1597b49abba8608af8
devUbaid <devubaid493@gmail.com>
2025-02-08 23:49:30 +0530

    Cors Origin

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 const corsConfig = {
-    origin: ["http://localhost:3000", "https://expense-analyzer-frontend.vercel.app"], 
+    origin: ["http://localhost:3000", "https://expense-analyzer-app.vercel.app"], 
     methods: ["GET", "POST", "PUT", "DELETE"], 
     allowedHeaders: ["Content-Type", "Authorization"], 
     credentials: true,
